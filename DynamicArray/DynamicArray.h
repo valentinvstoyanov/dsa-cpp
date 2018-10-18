431536c78e23e588df1d948520a4ef99916e37a3
valio_stoyanov <valentinstoqnov98@gmail.com>
2018-10-18 10:54:08 +0300

    Add Dynamic Array Implemetation

---
+++ DynamicArray/DynamicArray.h
@@ -0,0 +1,265 @@
+//
+// Created by valio_stoyanov on 10/12/18.
+//
+
+#ifndef DYNAMICARRAY_DYNAMICARRAY_H
+#define DYNAMICARRAY_DYNAMICARRAY_H
+
+#include <algorithm>
+#include <cassert>
+
+template <typename T>
+class DynamicArray {
+  T* buffer_;
+  size_t size_;
+  size_t capacity_;
+
+  void Copy(T* src, T* dest, size_t size) const;
+ public:
+  explicit DynamicArray(size_t = 1);
+  DynamicArray(const DynamicArray&);
+  ~DynamicArray();
+  DynamicArray& operator=(const DynamicArray&);
+
+  bool operator==(const DynamicArray<T>& other) const;
+  bool operator!=(const DynamicArray<T>& other) const;
+
+  DynamicArray<T>& operator+=(const DynamicArray<T>& other);
+  DynamicArray<T>& operator+=(const T& other);
+
+  void Clear();
+
+  void Resize(size_t);
+  void Reserve(size_t);
+  void ShrinkToFit();
+
+  bool Empty() const;
+
+  size_t Size() const;
+  size_t Capacity() const;
+
+  T& operator[] (size_t);
+  const T& operator[] (size_t) const;
+
+  T& At(size_t);
+  const T& At(size_t) const;
+
+  T& Front();
+  const T& Front() const;
+
+  T& Back();
+  const T& Back() const;
+
+  void PushBack(const T&);
+  void PopBack();
+};
+
+template<typename T>
+DynamicArray<T>::DynamicArray(size_t capacity)
+    : buffer_(nullptr), size_(0), capacity_(0) {
+  if (capacity == 0) {
+    Clear();
+    return;
+  }
+  buffer_ = new T[capacity];
+  capacity_ = capacity;
+}
+
+template<typename T>
+DynamicArray<T>::DynamicArray(const DynamicArray& other)
+    : buffer_(nullptr), size_(0), capacity_(0) {
+  Reserve(other.capacity_);
+  Copy(buffer_, other.buffer_, other.size_);
+  size_ = other.size_;
+}
+
+template<typename T>
+DynamicArray<T>::~DynamicArray() {
+  Clear();
+}
+
+template<typename T>
+DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray& other) {
+  if (this != &other) {
+    Clear();
+    if (other.capacity_ > 0) {
+      buffer_ = new T[other.capacity_];
+      Copy(buffer_, other.buffer_, other.size_);
+      size_ = other.size_;
+      capacity_ = other.capacity_;
+    }
+  }
+  return *this;
+}
+
+template<typename T>
+void DynamicArray<T>::Clear() {
+  delete[] buffer_;
+  size_ = 0;
+  capacity_ = 0;
+}
+
+template<typename T>
+void DynamicArray<T>::Resize(size_t size) {
+  if (size == 0) {
+    Clear();
+    return;
+  }
+
+  T* buffer = new T[size];
+  const size_t min_size = std::min(size, size_);
+  Copy(buffer, buffer_, min_size);
+
+  delete[] buffer_;
+  buffer_ = buffer;
+  size_ = min_size;
+  capacity_ = size;
+}
+
+template<typename T>
+void DynamicArray<T>::Reserve(size_t capacity) {
+  if (capacity <= capacity_)
+    return;
+
+  T* buffer = new T[capacity];
+  Copy(buffer, buffer_, size_);
+
+  delete[] buffer_;
+  buffer_ = buffer;
+  capacity_ = capacity;
+}
+
+template<typename T>
+void DynamicArray<T>::ShrinkToFit() {
+  if (size_ == 0) {
+    Clear();
+    return;
+  }
+
+  if (size_ == capacity_)
+    return;
+
+  T* buffer = new T[size_];
+  Copy(buffer, buffer_, size_);
+
+  delete[] buffer_;
+  capacity_ = size_;
+}
+
+template<typename T>
+bool DynamicArray<T>::Empty() const {
+  return size_ == 0;
+}
+
+template<typename T>
+size_t DynamicArray<T>::Size() const {
+  return size_;
+}
+
+template<typename T>
+size_t DynamicArray<T>::Capacity() const {
+  return capacity_;
+}
+
+template<typename T>
+T& DynamicArray<T>::operator[](size_t pos) {
+  assert(pos < size_ && "Index out of range");
+  return buffer_[pos];
+}
+
+template<typename T>
+const T& DynamicArray<T>::operator[](size_t pos) const {
+  assert(pos < size_ && "Index out of range");
+  return buffer_[pos];
+}
+
+template<typename T>
+T& DynamicArray<T>::At(size_t pos) {
+  assert(pos < size_ && "At called with pos out of range");
+  return buffer_[pos];
+}
+
+template<typename T>
+const T& DynamicArray<T>::At(size_t pos) const {
+  assert(pos < size_ && "At called with pos out of range");
+  return buffer_[pos];
+}
+
+template<typename T>
+T& DynamicArray<T>::Front() {
+  assert(size_ > 0 && "Front method called on empty array");
+  return buffer_[0];
+}
+
+template<typename T>
+const T& DynamicArray<T>::Front() const {
+  assert(size_ > 0 && "Front method called on empty array");
+  return buffer_[0];
+}
+
+template<typename T>
+T& DynamicArray<T>::Back() {
+  assert(size_ > 0 && "Back method called on empty array");
+  return buffer_[size_ - 1];
+}
+
+template<typename T>
+const T& DynamicArray<T>::Back() const {
+  assert(size_ > 0 && "Back method called on empty array");
+  return buffer_[size_ - 1];
+}
+
+template<typename T>
+void DynamicArray<T>::PushBack(const T& val) {
+  if (size_ == capacity_)
+    Resize(size_ == 0 ? 1 : size_ * 2);
+  buffer_[size_++] = val;
+}
+
+template<typename T>
+void DynamicArray<T>::PopBack() {
+  assert(size_ > 0 && "Pop method called on empty array");
+  --size_;
+}
+
+template<typename T>
+void DynamicArray<T>::Copy(T* dest, T* src, size_t size) const {
+  if (src == nullptr || dest == nullptr) return;
+  for (size_t i = 0; i < size; ++i)
+    dest[i] = src[i];
+}
+
+template<typename T>
+bool DynamicArray<T>::operator==(const DynamicArray<T>& other) const {
+  if (this == &other)
+    return true;
+  if (size_ != other.size_)
+    return false;
+
+  for (size_t i = 0; i < size_; ++i)
+    if (buffer_[i] != other.buffer_[i])
+      return false;
+
+  return true;
+}
+
+template<typename T>
+bool DynamicArray<T>::operator!=(const DynamicArray<T>& other) const {
+  return !(*this == other);
+}
+
+template<typename T>
+DynamicArray<T>& DynamicArray<T>::operator+=(const DynamicArray<T>& other) {
+  Resize(size_ + other.size_);
+  Copy(buffer_ + size_, other.buffer_, other.size_);
+  size_ += other.size_;
+  return *this;
+}
+
+template<typename T>
+DynamicArray<T>& DynamicArray<T>::operator+=(const T& other) {
+  PushBack(other);
+  return *this;
+}
+
+#endif //DYNAMICARRAY_DYNAMICARRAY_H
---
+++ DynamicArray/test.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "lib/catch.hpp"
+#include "DynamicArray.h"
+
+TEST_CASE( "DynamicArray with default ctor", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+  REQUIRE(arr.Size() == 0);
+  REQUIRE(arr.Capacity() == 1);
+  REQUIRE(arr.Empty());
+}
+
+TEST_CASE( "DynamicArray with initial capacity", "[DynamicArray]" ) {
+  DynamicArray<int> arr(100);
+  REQUIRE(arr.Size() == 0);
+  REQUIRE(arr.Capacity() == 100);
+  REQUIRE(arr.Empty());
+}
+
+TEST_CASE( "DynamicArray push 10 elements back", "[DynamicArray]" ) {
+  DynamicArray<int> arr(0);
+
+  for (unsigned i = 0; i < 10; ++i)
+    arr.PushBack(i);
+  REQUIRE(arr.Front() == 0);
+  REQUIRE(arr.Size() == 10);
+  REQUIRE(arr.Capacity() == 16);
+}
+
+TEST_CASE( "DynamicArray pop 10 elements", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+
+  for (unsigned i = 0; i < 10; ++i)
+    arr.PushBack(i);
+
+  for (unsigned i = 0; i < 10; ++i)
+    arr.PopBack();
+  REQUIRE(arr.Size() == 0);
+  REQUIRE(arr.Capacity() == 16);
+}
+
+TEST_CASE( "DynamicArray can show it's k-th element through operator[]", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+  for (unsigned i = 0; i < 10; ++i) {
+    arr.PushBack(i);
+    REQUIRE(arr[i] == i);
+  }
+}
+
+TEST_CASE( "DynamicArray can show it's k-th element", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+  for (unsigned i = 0; i < 10; ++i) {
+    arr.PushBack(i);
+    REQUIRE(arr.At(i) == i);
+  }
+}
+
+TEST_CASE( "DynamicArray can show it's first and last element", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+
+  SECTION( "10 elements" ) {
+    for (unsigned i = 0; i < 10; ++i)
+      arr.PushBack(i);
+    REQUIRE(arr.Front() == 0);
+    REQUIRE(arr.Back() == 9);
+  }
+
+  SECTION( "Single element" ) {
+    arr.PushBack(1000);
+    REQUIRE(arr.Front() == 1000);
+    REQUIRE(arr.Back() == 1000);
+  }
+}
+
+TEST_CASE( "DynamicArray can be cleared", "[DynamicArray]" ) {
+  DynamicArray<int> arr(0);
+
+  SECTION( "On empty array with zero capacity" ) {
+    arr.Clear();
+    REQUIRE(arr.Empty());
+    REQUIRE(arr.Size() == 0);
+    REQUIRE(arr.Capacity() == 0);
+  }
+
+  SECTION( "On non-empty array" ) {
+    for (unsigned i = 0; i < 10; ++i)
+      arr.PushBack(i + 1000);
+
+    arr.Clear();
+    REQUIRE(arr.Empty());
+    REQUIRE(arr.Size() == 0);
+    REQUIRE(arr.Capacity() == 0);
+  }
+
+  SECTION( "On empty array" ) {
+    arr.Clear();
+    REQUIRE(arr.Empty());
+    REQUIRE(arr.Size() == 0);
+    REQUIRE(arr.Capacity() == 0);
+  }
+}
+
+TEST_CASE( "DynamicArray can be shrinked to fit size", "[DynamicArray]" ) {
+  SECTION( "On empty array with zero capacity" ) {
+    DynamicArray<int> arr(0);
+    arr.ShrinkToFit();
+    REQUIRE(arr.Size() == arr.Capacity());
+  }
+
+  SECTION( "On empty array with non-zero capacity" ) {
+    DynamicArray<int> arr(100);
+    arr.ShrinkToFit();
+    REQUIRE(arr.Size() == arr.Capacity());
+  }
+
+  SECTION( "On non-empty array" ) {
+    DynamicArray<int> arr;
+    for (unsigned i = 0; i < 100; ++i)
+      arr.PushBack(i);
+    arr.ShrinkToFit();
+    REQUIRE(arr.Size() == arr.Capacity());
+  }
+}
+
+TEST_CASE( "DynamicArray can reserve capacity", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+
+  SECTION( "On empty array with zero capacity" ) {
+    arr.Clear();
+    arr.Reserve(100);
+    REQUIRE(arr.Capacity() == 100);
+  }
+
+  SECTION( "On non-empty array" ) {
+    for (unsigned i = 0; i < 10; ++i)
+      arr.PushBack(i);
+    arr.Reserve(100);
+    REQUIRE(arr.Capacity() == 100);
+  }
+
+  SECTION( "On empty array with non-zero capacity" ) {
+    arr.Reserve(100);
+    REQUIRE(arr.Capacity() == 100);
+  }
+}
+
+TEST_CASE( "DynamicArray copy ctor", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+
+  SECTION( "On empty array" ) {
+    DynamicArray<int> arrCopy(arr);
+    REQUIRE(arrCopy.Size() == arr.Size());
+    REQUIRE(arrCopy.Capacity() == arr.Capacity());
+  }
+
+  SECTION( "On empty array with non-zero capacity" ) {
+    arr.Clear();
+    DynamicArray<int> arrCopy(arr);
+    REQUIRE(arrCopy.Size() == arr.Size());
+    REQUIRE(arrCopy.Capacity() == arr.Capacity());
+  }
+
+  SECTION( "On non-empty array" ) {
+    for (unsigned i = 0; i < 10; ++i)
+      arr.PushBack(i);
+    DynamicArray<int> arrCopy(arr);
+    REQUIRE(arrCopy.Size() == arr.Size());
+    REQUIRE(arrCopy.Capacity() == arr.Capacity());
+    for (unsigned i = 0; i < 10; ++i)
+      REQUIRE(arrCopy[i] == arr[i]);
+  }
+}
+
+TEST_CASE( "DynamicArray operator==", "[DynamicArray]" ) {
+  SECTION( "On empty array with non-zero capacity" ) {
+    DynamicArray<int> da1;
+    DynamicArray<int> da2(0);
+    da2 = da1;
+    REQUIRE(da2.Size() == da1.Size());
+    REQUIRE(da2.Capacity() == da1.Capacity());
+  }
+
+  SECTION( "On non-empty array" ) {
+    DynamicArray<int> da1;
+    DynamicArray<int> da2(0);
+    da1.PushBack(10);
+    da2.PushBack(110);
+    da2 = da1;
+    REQUIRE(da2.Size() == da1.Size());
+    REQUIRE(da2.Capacity() == da1.Capacity());
+    REQUIRE(da2.Front() == da1.Front());
+  }
+}
+
+TEST_CASE( "DynamicArrays comparison", "[DynamicArray]" ) {
+  DynamicArray<int> da1(10);
+  DynamicArray<int> da2(10);
+  for(unsigned i = 0; i < 10; ++i) {
+    da1.PushBack(i);
+    da2.PushBack(i);
+  }
+
+  REQUIRE(da1 == da2);
+  REQUIRE_FALSE(da1 != da2);
+
+  da2.Front() = 1;
+  REQUIRE_FALSE(da1 == da2);
+  REQUIRE(da1 != da2);
+
+  da1.PopBack();
+  REQUIRE_FALSE(da1 == da2);
+  REQUIRE(da1 != da2);
+}
+
+TEST_CASE( "DynamicArray append other DynamicArray", "[DynamicArray]" ) {
+  DynamicArray<int> da1(10);
+  DynamicArray<int> da2(10);
+  for(unsigned i = 0; i < 10; ++i) {
+    da1.PushBack(i);
+    da2.PushBack(i + 10);
+  }
+
+  da1 += da2;
+  REQUIRE(da1.Size() == 20);
+  REQUIRE(da1.Back() == 19);
+}
+
+TEST_CASE( "DynamicArray append element", "[DynamicArray]" ) {
+  DynamicArray<int> arr;
+  arr += 1;
+  REQUIRE(arr.Size() == 1);
+  REQUIRE(arr.Back() == 1);
+}
