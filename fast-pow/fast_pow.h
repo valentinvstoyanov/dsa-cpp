50000062dd192ad92d43dacd45f20567168e4167
valio_stoyanov <valentinstoqnov98@gmail.com>
2018-10-20 09:30:37 +0300

    Rename method in fast exp algo

--- fast-pow/fast_pow.h
+++ fast-pow/fast_pow.h
@@ -5,9 +5,9 @@
 #ifndef FAST_POW_FAST_POW_H
 #define FAST_POW_FAST_POW_H
 
-long long fast_pow(long long a, long long n) {
+long long FastPow(long long a, long long n) {
   if (n == 0) return 1;
-  long long x = fast_pow(a, n / 2);
+  long long x = FastPow(a, n / 2);
   if (n % 2 == 0) return x * x;
   else return a * x * x;
 }
--- fast-pow/test.cpp
+++ fast-pow/test.cpp
@@ -2,19 +2,19 @@
 #include "lib/catch.hpp"
 #include "fast_pow.h"
 
-TEST_CASE("Fast power with positive integers", "[fast_pow]") {
-  REQUIRE(fast_pow(10, 2) == 100);
-  REQUIRE(fast_pow(2, 10) == 1024);
-  REQUIRE(fast_pow(5, 4) == 625);
+TEST_CASE("Fast power with positive integers", "[FastPow]") {
+  REQUIRE(FastPow(10, 2) == 100);
+  REQUIRE(FastPow(2, 10) == 1024);
+  REQUIRE(FastPow(5, 4) == 625);
 }
 
-TEST_CASE("Fast power with 0 power", "[fast_pow]") {
-  REQUIRE(fast_pow(10, 0) == 1);
-  REQUIRE(fast_pow(2, 0) == 1);
-  REQUIRE(fast_pow(-5, 0) == 1);
-  REQUIRE(fast_pow(50000000, 0) == 1);
+TEST_CASE("Fast power with 0 power", "[FastPow]") {
+  REQUIRE(FastPow(10, 0) == 1);
+  REQUIRE(FastPow(2, 0) == 1);
+  REQUIRE(FastPow(-5, 0) == 1);
+  REQUIRE(FastPow(50000000, 0) == 1);
 }
 
-TEST_CASE("Fast power with negative base", "[fast_pow]") {
-  REQUIRE(fast_pow(-10, 2) == 100);
+TEST_CASE("Fast power with negative base", "[FastPow]") {
+  REQUIRE(FastPow(-10, 2) == 100);
 }
