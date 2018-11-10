68ff6690021afaf62af12587f29ecc8cdee88364
valio_stoyanov <valentinstoqnov98@gmail.com>
2018-11-10 10:37:34 +0200

    DLL fixes and added more tests

--- data-structures/linked-list/cyclic-doubly-linked-list/doubly_linked_list.h
+++ data-structures/linked-list/cyclic-doubly-linked-list/doubly_linked_list.h
@@ -12,8 +12,9 @@ template<typename T>
 class DoublyLinkedList {
   struct Node {
     T data_;
-    Node* next;
+    Node* next_;
     Node* prev_;
+    Node() = default;
     explicit Node(T data);
   } sentinel_;
   size_t size_;
@@ -25,10 +26,16 @@ class DoublyLinkedList {
   DoublyLinkedList(const DoublyLinkedList&);
   DoublyLinkedList& operator=(const DoublyLinkedList&);
 
+  bool operator==(const DoublyLinkedList&) const;
+  bool operator!=(const DoublyLinkedList&) const;
+  DoublyLinkedList& operator+=(const DoublyLinkedList&);
+  DoublyLinkedList& operator+=(const T&);
+
   void PushBack(const T&);
   void PushFront(const T&);
   void PopBack();
   void PopFront();
+
   T& Back();
   T& Front();
   const T& Back() const;
@@ -46,17 +53,17 @@ DoublyLinkedList<T>::Node::Node(T data)
 
 template<typename T>
 void DoublyLinkedList<T>::CopyList(const DoublyLinkedList& other) {
-  Node* o_current = other.sentinel_.next;
+  Node* o_current = other.sentinel_.next_;
   while (o_current != &other.sentinel_) {
     PushBack(o_current->data_);
-    o_current = o_current->next;
+    o_current = o_current->next_;
   }
 }
 
 template<typename T>
 DoublyLinkedList<T>::DoublyLinkedList()
     : sentinel_(Node()), size_(0) {
-  sentinel_.prev_ = sentinel_.next = &sentinel_;
+  sentinel_.prev_ = sentinel_.next_ = &sentinel_;
 }
 
 template<typename T>
@@ -67,7 +74,7 @@ DoublyLinkedList<T>::~DoublyLinkedList() {
 template<typename T>
 DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList& other)
     : sentinel_(Node()), size_(0) {
-  sentinel_.prev_ = sentinel_.next = &sentinel_;
+  sentinel_.prev_ = sentinel_.next_ = &sentinel_;
   CopyList(other);
 }
 
@@ -84,8 +91,8 @@ template<typename T>
 void DoublyLinkedList<T>::PushBack(const T& val) {
   Node* node = new Node(val);
   node->prev_ = sentinel_.prev_;
-  node->next = &sentinel_;
-  sentinel_.prev_->next = node;
+  node->next_ = &sentinel_;
+  sentinel_.prev_->next_ = node;
   sentinel_.prev_ = node;
   ++size_;
 }
@@ -93,10 +100,10 @@ void DoublyLinkedList<T>::PushBack(const T& val) {
 template<typename T>
 void DoublyLinkedList<T>::PushFront(const T& val) {
   Node* node = new Node(val);
-  node->next = sentinel_.next;
+  node->next_ = sentinel_.next_;
   node->prev_ = &sentinel_;
-  sentinel_.next->prev_ = node;
-  sentinel_.next = node;
+  sentinel_.next_->prev_ = node;
+  sentinel_.next_ = node;
   ++size_;
 }
 
@@ -104,7 +111,7 @@ template<typename T>
 void DoublyLinkedList<T>::PopBack() {
   assert(size_ > 0 && "PopBack on empty linked list.");
   Node* node_to_delete = sentinel_.prev_;
-  node_to_delete->prev_->next = &sentinel_;
+  node_to_delete->prev_->next_ = &sentinel_;
   sentinel_.prev_ = node_to_delete->prev_;
   delete node_to_delete;
   --size_;
@@ -113,9 +120,9 @@ void DoublyLinkedList<T>::PopBack() {
 template<typename T>
 void DoublyLinkedList<T>::PopFront() {
   assert(size_ > 0 && "PopFront on empty linked list.");
-  Node* node_to_delete = sentinel_.next;
-  node_to_delete->next->prev_ = &sentinel_;
-  sentinel_.next = node_to_delete->next;
+  Node* node_to_delete = sentinel_.next_;
+  node_to_delete->next_->prev_ = &sentinel_;
+  sentinel_.next_ = node_to_delete->next_;
   delete node_to_delete;
   --size_;
 }
@@ -128,7 +135,7 @@ T& DoublyLinkedList<T>::Back() {
 template<typename T>
 T& DoublyLinkedList<T>::Front() {
   assert(size_ > 0 && "Front on empty linked list.");
-  return sentinel_.next->data_;
+  return sentinel_.next_->data_;
 }
 
 template<typename T>
@@ -139,7 +146,7 @@ const T& DoublyLinkedList<T>::Back() const {
 template<typename T>
 const T& DoublyLinkedList<T>::Front() const {
   assert(size_ > 0 && "Front on empty linked list.");
-  return sentinel_.next->data_;
+  return sentinel_.next_->data_;
 }
 
 template<typename T>
@@ -157,6 +164,57 @@ void DoublyLinkedList<T>::Clear() {
   while (!Empty())
     PopFront();
   size_ = 0;
+}
+
+template<typename T>
+bool DoublyLinkedList<T>::operator==(const DoublyLinkedList& other) const {
+  if (this == &other)
+    return true;
+  if (size_ != other.size_)
+    return false;
+
+  Node* this_current = sentinel_.next_;
+  Node* other_current = other.sentinel_.next_;
+  for (size_t i = 0; i < size_; ++i) {
+    if (this_current->data_ != other_current->data_)
+      return false;
+    this_current = this_current->next_;
+    other_current = other_current->next_;
+  }
+
+  return true;
+}
+
+template<typename T>
+bool DoublyLinkedList<T>::operator!=(const DoublyLinkedList& other) const {
+  return !(*this == other);
+}
+
+template<typename T>
+DoublyLinkedList<T>& DoublyLinkedList<T>::operator+=(const DoublyLinkedList& other) {
+  if (size_ == 0)
+    return *this = other;
+  if (!other.Empty()) {
+    Node* last = sentinel_.prev_;
+    Node* other_current = other.sentinel_.next_;
+    while (other_current != nullptr) {
+      Node* node = new Node(other_current->data_);
+      node->prev_ = last;
+      last->next_ = node;
+      last = last->next_;
+      other_current = other_current->next_;
+      ++size_;
+    }
+    last->next_ = &sentinel_;
+    sentinel_.prev_ = last;
+  }
+  return *this;
+}
+
+template<typename T>
+DoublyLinkedList<T>& DoublyLinkedList<T>::operator+=(const T& val) {
+  PushBack(val);
+  return *this;
 }
 
 #endif //CYCLIC_DOUBLY_LINKED_LIST_DOUBLY_LINKED_LIST_H
---
+++ data-structures/linked-list/cyclic-doubly-linked-list/test.cc
@@ -0,0 +1,203 @@
+#define CATCH_CONFIG_MAIN
+#include "lib/catch.hpp"
+#include "doubly_linked_list.h"
+#define DS " DoublyLinkedList "
+
+TEST_CASE(DS "can be created with default ctor", DS) {
+  REQUIRE_NOTHROW(DoublyLinkedList<int>());
+}
+
+TEST_CASE("Empty() returns true on empty" DS, DS) {
+  REQUIRE(DoublyLinkedList<char>().Empty());
+}
+
+TEST_CASE("Size() returns zero on empty" DS, DS) {
+  REQUIRE(DoublyLinkedList<char>().Size() == 0);
+}
+
+TEST_CASE("Clear() should not throw when called on empty" DS, DS) {
+  REQUIRE_NOTHROW(DoublyLinkedList<char>().Clear());
+}
+
+TEST_CASE("Push elements to" DS "should not throw", DS) {
+  DoublyLinkedList<unsigned> ds;
+  const unsigned start = 1;
+  const unsigned end = 129;
+
+  SECTION("Push front elements") {
+    for (unsigned i = start; i <= end; ++i)
+      REQUIRE_NOTHROW(ds.PushFront(i));
+    REQUIRE_FALSE(ds.Empty());
+    REQUIRE(ds.Size() == end - start + 1);
+  }
+  SECTION("Push back elements") {
+    for (unsigned i = start; i <= end; ++i)
+      REQUIRE_NOTHROW(ds.PushBack(i));
+    REQUIRE(ds.Size() == end - start + 1);
+    REQUIRE_FALSE(ds.Empty());
+  }
+  SECTION("Push front and back elements") {
+    for (unsigned i = start; i <= end; ++i)
+      if (i > 0 && i % 2 == 0) REQUIRE_NOTHROW(ds.PushBack(i));
+      else
+        REQUIRE_NOTHROW(ds.PushFront(i));
+    REQUIRE(ds.Size() == end - start + 1);
+    REQUIRE_FALSE(ds.Empty());
+  }
+}
+
+TEST_CASE("Front() and Back() should work", DS) {
+  DoublyLinkedList<int> ds;
+  const unsigned e1 = 0, e2 = 1, e3 = 2;
+  ds.PushFront(e1); ds.PushBack(e2); ds.PushBack(e3);
+  SECTION("Test Front()") { REQUIRE(ds.Front() == e1); }
+  SECTION("Test Back()") { REQUIRE(ds.Back() == e3); }
+}
+
+TEST_CASE("Pop elements from" DS "should not throw", DS) {
+  const int test_arr_size = 5;
+  const int test_arr[test_arr_size] = {5, 1, 2, 4, 3};
+  DoublyLinkedList<int> ds;
+  for (int i = 0; i < test_arr_size; ++i)
+    ds.PushBack(test_arr[i]);
+
+  SECTION("Pop back elements") {
+    for (int i = 0; i < test_arr_size; ++i) {
+      REQUIRE(ds.Back() == test_arr[test_arr_size - i - 1]);
+      REQUIRE_NOTHROW(ds.PopBack());
+    }
+    REQUIRE(ds.Empty());
+    REQUIRE(ds.Size() == 0);
+  }
+  SECTION("Pop front elements") {
+    for (int i = 0; i < test_arr_size; ++i) {
+      REQUIRE(ds.Front() == test_arr[i]);
+      REQUIRE_NOTHROW(ds.PopFront());
+    }
+    REQUIRE(ds.Empty());
+    REQUIRE(ds.Size() == 0);
+  }
+}
+
+TEST_CASE("Clear() non-empty" DS, DS) {
+  DoublyLinkedList<int> ds;
+  const unsigned e1 = 0, e2 = 1, e3 = 2;
+  ds.PushFront(e1); ds.PushBack(e2); ds.PushBack(e3);
+
+  SECTION("Once") {
+    ds.Clear();
+    REQUIRE(ds.Empty());
+    REQUIRE(ds.Size() == 0);
+  }
+  SECTION("More times") {
+    for (int i = 0; i < 10; ++i) {
+      REQUIRE_NOTHROW(ds.Clear());
+      REQUIRE(ds.Empty());
+      REQUIRE(ds.Size() == 0);
+    }
+  }
+}
+
+TEST_CASE(DS "comparison", DS) {
+  DoublyLinkedList<int> ds1;
+  DoublyLinkedList<int> ds2;
+
+  SECTION("On empty" DS "should be true") {
+    REQUIRE(ds1 == ds2);
+    REQUIRE_FALSE(ds1 != ds2);
+  }
+  SECTION("On non-empty" DS "with same elements should be true") {
+    for (unsigned i = 0; i < 10; ++i) {
+      ds1.PushBack(i);
+      ds2.PushBack(i);
+    }
+    REQUIRE(ds1 == ds2);
+    REQUIRE_FALSE(ds1 != ds2);
+  }
+  SECTION("On non-empty" DS "with different elements should be false") {
+    for (unsigned i = 0; i < 10; ++i) {
+      ds1.PushBack(i);
+      ds2.PushBack(i + 10);
+    }
+    REQUIRE_FALSE(ds1 == ds2);
+    REQUIRE(ds1 != ds2);
+  }
+}
+
+TEST_CASE(DS "can be created with copy ctor", DS) {
+  SECTION("From empty" DS) {
+    DoublyLinkedList<int> empty_ds;
+    DoublyLinkedList<int> ds(empty_ds);
+    REQUIRE(ds == empty_ds);
+  }
+  SECTION("From non-empty" DS) {
+    DoublyLinkedList<int> ds;
+    const unsigned e1 = 0, e2 = 1, e3 = 2;
+    ds.PushFront(e1); ds.PushBack(e2); ds.PushBack(e3);
+
+    DoublyLinkedList<int> copy_of_ds(ds);
+    REQUIRE(copy_of_ds == ds);
+  }
+}
+
+TEST_CASE(DS "operator=", DS) {
+  SECTION("On empty" DS) {
+    DoublyLinkedList<int> ds1;
+    DoublyLinkedList<int> ds2;
+    ds2 = ds1;
+    REQUIRE(ds2 == ds1);
+  }
+  SECTION("On non-empty" DS) {
+    DoublyLinkedList<int> ds1;
+    DoublyLinkedList<int> ds2;
+    ds1.PushBack(10);
+    ds2.PushBack(110);
+    ds2 = ds1;
+    REQUIRE(ds2 == ds1);
+  }
+}
+
+TEST_CASE(DS "operator+= should append correctly", DS) {
+  SECTION("When both are empty") {
+    DoublyLinkedList<int> empty_ds;
+    DoublyLinkedList<int> ds;
+    ds += empty_ds;
+    REQUIRE(ds == empty_ds);
+  }
+  SECTION("When lhs is empty") {
+    DoublyLinkedList<int> lhs;
+    DoublyLinkedList<int> rhs;
+    const int rhs_elements_count = 10;
+    for (int i = 0; i < rhs_elements_count; ++i)
+      rhs.PushBack(i);
+    lhs += rhs;
+    REQUIRE(lhs == rhs);
+  }
+  SECTION("When rhs is empty") {
+    DoublyLinkedList<int> lhs;
+    DoublyLinkedList<int> rhs;
+    const int lhs_elements_count = 10;
+    for (int i = 0; i < lhs_elements_count; ++i)
+      lhs.PushBack(i);
+    DoublyLinkedList<int> lhs_copy(lhs);
+    lhs += rhs;
+    REQUIRE(lhs == lhs_copy);
+  }
+  SECTION("Single element append when" DS " is empty") {
+    const int element = 10;
+    DoublyLinkedList<int> ds;
+    ds += element;
+    REQUIRE(ds.Size() == 1);
+    REQUIRE(ds.Back() == element);
+    REQUIRE(ds.Front() == element);
+  }
+  SECTION("Single element append when" DS " is empty") {
+    const unsigned element = 10;
+    DoublyLinkedList<unsigned > ds;
+    for (unsigned i = 0; i < element; ++i)
+      ds.PushFront(i);
+    ds += element;
+    REQUIRE(ds.Size() == element + 1);
+    REQUIRE(ds.Back() == element);
+  }
+}
