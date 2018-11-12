44a3e1e4a27030df886df74844ff925130ab461c
valio_stoyanov <valentinstoqnov98@gmail.com>
2018-11-12 08:37:48 +0200

    Add stack using ll impl

---
+++ data-structures/stack/stack-using-linked-list/stack.h
@@ -0,0 +1,150 @@
+//
+// Created by valio_stoyanov on 11/12/18.
+//
+
+#ifndef STACK_USING_LINKED_LIST_STACK_H
+#define STACK_USING_LINKED_LIST_STACK_H
+
+#include <cassert>
+#include <cstddef>
+
+template<typename T>
+class Stack {
+  struct Node {
+    T data_;
+    Node* next_;
+    explicit Node(T data);
+  };
+  Node* top_;
+  size_t size_;
+  void CopyStack(const Stack&);
+ public:
+  Stack();
+  Stack(const Stack&);
+  ~Stack();
+
+  Stack& operator=(const Stack&);
+  Stack& operator+=(const Stack&);
+  Stack& operator+=(const T&);
+  bool operator==(const Stack&) const;
+  bool operator!=(const Stack&) const;
+
+  bool Empty() const;
+  size_t Size() const;
+
+  void Push(const T&);
+  T Pop();
+
+  void Clear();
+};
+
+template<typename T>
+Stack<T>::Node::Node(T data)
+    : data_(data), next_(nullptr) {}
+
+template<typename T>
+void Stack<T>::CopyStack(const Stack& other) {
+  if (other.Empty()) return;
+  Push(other.top_->data_);
+  Node* other_curr = other.top_;
+  Node* this_curr = top_;
+  while (other_curr->next_) {
+    this_curr->next_ = new Node(other_curr->next_->data_);
+    other_curr = other_curr->next_;
+    this_curr = this_curr->next_;
+    ++size_;
+  }
+}
+
+template<typename T>
+Stack<T>::Stack()
+    : top_(nullptr), size_(0) {}
+
+template<typename T>
+Stack<T>::Stack(const Stack& other)
+    : top_(nullptr), size_(0) {
+  CopyStack(other);
+}
+
+template<typename T>
+Stack<T>::~Stack() {
+  Clear();
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator=(const Stack& other) {
+  if (this != &other) {
+    Clear();
+    CopyStack(other);
+  }
+  return *this;
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator+=(const Stack& rhs) {
+  //TODO
+  return *this;
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator+=(const T& val) {
+  Push(val);
+  return *this;
+}
+
+template<typename T>
+bool Stack<T>::operator==(const Stack& other) const {
+  if (this == &other) return true;
+  if (size_ != other.size_) return false;
+
+  Node* this_curr = top_;
+  Node* other_curr = other.top_;
+  for (size_t i = 0; i < size_; ++i) {
+    if (this_curr->data_ != other_curr->data_) return false;
+    this_curr = this_curr->next_;
+    other_curr = other_curr->next_;
+  }
+
+  return true;
+}
+
+template<typename T>
+bool Stack<T>::operator!=(const Stack& other) const {
+  return !(*this == other);
+}
+
+template<typename T>
+bool Stack<T>::Empty() const {
+  return top_ == nullptr;
+}
+
+template<typename T>
+size_t Stack<T>::Size() const {
+  return size_;
+}
+
+template<typename T>
+void Stack<T>::Push(const T& val) {
+  Node* node = new Node(val);
+  node->next_ = top_;
+  top_ = node;
+  ++size_;
+}
+
+template<typename T>
+T Stack<T>::Pop() {
+  assert(!Empty() && "Pop called on empty stack");
+  Node* first = top_;
+  T result = first->data_;
+  top_ = first->next_;
+  delete first;
+  --size_;
+  return result;
+}
+
+template<typename T>
+void Stack<T>::Clear() {
+  while (!Empty()) Pop();
+}
+
+#endif //STACK_USING_LINKED_LIST_STACK_H
---
+++ data-structures/stack/stack-using-linked-list/test.cc
@@ -0,0 +1,165 @@
+#define CATCH_CONFIG_MAIN
+#include "lib/catch.hpp"
+#include "stack.h"
+#define DS " Stack "
+
+TEST_CASE(DS "can be created with default ctor", DS) {
+  REQUIRE_NOTHROW(Stack<int>());
+}
+
+TEST_CASE("Empty() returns true on empty" DS, DS) {
+  REQUIRE(Stack<char>().Empty());
+}
+
+TEST_CASE("Size() returns zero on empty" DS, DS) {
+  REQUIRE(Stack<char>().Size() == 0);
+}
+
+TEST_CASE("Clear() should not throw when called on empty" DS, DS) {
+  REQUIRE_NOTHROW(Stack<char>().Clear());
+}
+
+TEST_CASE("Push elements to" DS "should not throw", DS) {
+  Stack<unsigned> ds;
+  const unsigned start = 1;
+  const unsigned end = 129;
+  for (unsigned i = start; i <= end; ++i)
+    REQUIRE_NOTHROW(ds.Push(i));
+  REQUIRE(ds.Size() == end - start + 1);
+  REQUIRE_FALSE(ds.Empty());
+}
+
+TEST_CASE("Pop elements from" DS "should not throw", DS) {
+  const int test_arr_size = 5;
+  const int test_arr[test_arr_size] = {5, 1, 2, 4, 3};
+  Stack<int> ds;
+  for (int i = 0; i < test_arr_size; ++i)
+    ds.Push(test_arr[i]);
+  for (int i = 0; i < test_arr_size; ++i)
+    REQUIRE(ds.Pop() == test_arr[test_arr_size - i - 1]);
+  REQUIRE(ds.Empty());
+  REQUIRE(ds.Size() == 0);
+}
+
+TEST_CASE("Clear() non-empty" DS, DS) {
+  Stack<int> ds;
+  const unsigned e1 = 0, e2 = 1, e3 = 2;
+  ds.Push(e1); ds.Push(e2); ds.Push(e3);
+
+  SECTION("Once") {
+    ds.Clear();
+    REQUIRE(ds.Empty());
+    REQUIRE(ds.Size() == 0);
+  }
+  SECTION("More times") {
+    for (int i = 0; i < 10; ++i) {
+      REQUIRE_NOTHROW(ds.Clear());
+      REQUIRE(ds.Empty());
+      REQUIRE(ds.Size() == 0);
+    }
+  }
+}
+
+TEST_CASE(DS "comparison", DS) {
+  Stack<int> ds1;
+  Stack<int> ds2;
+
+  SECTION("On empty" DS "should be true") {
+    REQUIRE(ds1 == ds2);
+    REQUIRE_FALSE(ds1 != ds2);
+  }
+  SECTION("On non-empty" DS "with same elements should be true") {
+    for (unsigned i = 0; i < 10; ++i) {
+      ds1.Push(i);
+      ds2.Push(i);
+    }
+    REQUIRE(ds1 == ds2);
+    REQUIRE_FALSE(ds1 != ds2);
+  }
+  SECTION("On non-empty" DS "with different elements should be false") {
+    for (unsigned i = 0; i < 10; ++i) {
+      ds1.Push(i);
+      ds2.Push(i + 10);
+    }
+    REQUIRE_FALSE(ds1 == ds2);
+    REQUIRE(ds1 != ds2);
+  }
+}
+
+TEST_CASE(DS "can be created with copy ctor", DS) {
+  SECTION("From empty" DS) {
+    Stack<int> empty_ds;
+    Stack<int> ds(empty_ds);
+    REQUIRE(ds == empty_ds);
+  }
+  SECTION("From non-empty" DS) {
+    Stack<int> ds;
+    const unsigned e1 = 0, e2 = 1, e3 = 2;
+    ds.Push(e1); ds.Push(e2); ds.Push(e3);
+
+    Stack<int> copy_of_ds(ds);
+    REQUIRE(copy_of_ds == ds);
+  }
+}
+
+TEST_CASE(DS "operator=", DS) {
+  SECTION("On empty" DS) {
+    Stack<int> ds1;
+    Stack<int> ds2;
+    ds2 = ds1;
+    REQUIRE(ds2 == ds1);
+  }
+  SECTION("On non-empty" DS) {
+    Stack<int> ds1;
+    Stack<int> ds2;
+    ds1.Push(10);
+    ds2.Push(110);
+    ds2 = ds1;
+    REQUIRE(ds2 == ds1);
+  }
+}
+
+TEST_CASE(DS "operator+= should append correctly", DS) {
+  SECTION("When both are empty") {
+    Stack<int> empty_ds;
+    Stack<int> ds;
+    ds += empty_ds;
+    REQUIRE(ds == empty_ds);
+  }
+  SECTION("When lhs is empty") {
+    //Feature not yet implemented
+    Stack<int> lhs;
+    Stack<int> rhs;
+    const int rhs_elements_count = 10;
+    for (int i = 0; i < rhs_elements_count; ++i)
+      rhs.Push(i);
+    lhs += rhs;
+    REQUIRE(lhs == rhs);
+  }
+  SECTION("When rhs is empty") {
+    Stack<int> lhs;
+    Stack<int> rhs;
+    const int lhs_elements_count = 10;
+    for (int i = 0; i < lhs_elements_count; ++i)
+      lhs.Push(i);
+    Stack<int> lhs_copy(lhs);
+    lhs += rhs;
+    REQUIRE(lhs == lhs_copy);
+  }
+  SECTION("Single element append when" DS " is empty") {
+    const int element = 10;
+    Stack<int> ds;
+    ds += element;
+    REQUIRE(ds.Size() == 1);
+    REQUIRE(ds.Pop() == element);
+  }
+  SECTION("Single element append when" DS " is empty") {
+    const unsigned element = 10;
+    Stack<unsigned > ds;
+    for (unsigned i = 0; i < element; ++i)
+      ds.Push(i);
+    ds += element;
+    REQUIRE(ds.Size() == element + 1);
+    REQUIRE(ds.Pop() == element);
+  }
+}
