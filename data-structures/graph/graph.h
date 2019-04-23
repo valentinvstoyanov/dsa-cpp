078a0d8d4a498afafe39a3e92ab91812dd9d66e2
valentinvstoyanov <valentinstoqnov98@gmail.com>
2019-04-23 07:10:40 +0300

    Add graph with bfs and dfs

---
+++ data-structures/graph/graph.h
@@ -0,0 +1,179 @@
+#ifndef GRAPH_GRAPH_H
+#define GRAPH_GRAPH_H
+
+#include <unordered_map>
+#include <functional>
+#include <unordered_set>
+#include <queue>
+#include <cassert>
+#include <iostream>
+
+template<typename V = int, typename E = int>
+class Graph {
+  using EdgeMap = std::unordered_map<V, E>;
+  using VertexMap = std::unordered_map<V, EdgeMap>;
+
+  using VisitCallback = std::function<bool(const V&)>;
+
+  VertexMap vertex_map_;
+  size_t edge_count_;
+
+  bool BFSVisitHelper(const V& start, const VisitCallback& visit, std::unordered_set<V>& visited) const {
+    std::queue<V> q;
+    q.push(start);
+    visited.insert(start);
+
+    while (!q.empty()) {
+      const V current = q.front();
+      q.pop();
+
+      if (!visit(current))
+        return true;
+
+      const EdgeMap& adj = vertex_map_.find(current)->second;
+      for (const auto& element: adj) {
+        const V& vertex = element.first;
+
+        if (visited.find(vertex) == visited.end()) {
+          q.push(vertex);
+          visited.insert(vertex);
+        }
+      }
+    }
+
+    return true;
+  }
+
+  bool DFSVisitHelper(const V& start, const VisitCallback& visit, std::unordered_set<V>& visited) const {
+    if (visited.find(start) != visited.end())
+      return true;
+
+    visited.insert(start);
+    if (!visit(start))
+      return false;
+
+    const EdgeMap& adj = vertex_map_.find(start)->second;
+    for (const auto& element: adj) {
+      const V& vertex = element.first;
+
+      if (visited.find(vertex) == visited.end())
+        if (!DFSVisitHelper(vertex, visit, visited))
+          return false;
+    }
+
+    return true;
+  }
+
+ public:
+  bool AddVertex(const V& vertex) {
+    if (vertex_map_.find(vertex) == vertex_map_.end()) {
+      vertex_map_.insert(std::make_pair(vertex, EdgeMap()));
+      return true;
+    }
+
+    return false;
+  }
+
+  bool AddEdge(const V& from, const V& to, const E& edge) {
+    auto vertex_it = vertex_map_.find(from);
+    if (vertex_it == vertex_map_.end() || vertex_map_.find(to) == vertex_map_.end())
+      return false;
+
+    vertex_it->second[to] = edge;
+    ++edge_count_;
+    return true;
+  }
+
+  bool RemoveVertex(const V& vertex) {
+    auto vertex_it = vertex_map_.find(vertex);
+    if (vertex_it == vertex_map_.end())
+      return false;
+
+    const size_t adjacent_count = vertex_it->second.size();
+    vertex_map_.erase(vertex_it);
+
+    for (auto& element: vertex_map_) {
+      EdgeMap& adjacent = element.second;
+      auto edge_it = adjacent.find(element.first);
+      if (edge_it != adjacent.end()) {
+        adjacent.erase(edge_it);
+        --edge_count_;
+      }
+    }
+
+    return true;
+  }
+
+  bool RemoveEdge(const V& from, const V& to) {
+    auto vertex_it = vertex_map_.find(from);
+    if (vertex_it == vertex_map_.end() || vertex_map_.find(to) == vertex_map_.end())
+      return false;
+
+    if (vertex_it->second.erase(to) != 0) {
+      --edge_count_;
+      return true;
+    }
+
+    return false;
+  }
+
+  void BFS(const VisitCallback& visit) const {
+    std::unordered_set<V> visited;
+
+    for (const auto& vit: vertex_map_)
+      if (visited.find(vit.first) == visited.end())
+        if (!BFSVisitHelper(vit.first, visit, visited))
+          return;
+  }
+
+  bool BFSVisit(const V& start, const VisitCallback& visit) const {
+    if (vertex_map_.find(start) == vertex_map_.end())
+      return false;
+
+    std::unordered_set<V> visited;
+    BFSVisitHelper(start, visit, visited);
+    return true;
+  }
+
+  void DFS(const VisitCallback& visit) const {
+    std::unordered_set<V> visited;
+
+    for (const auto& vit: vertex_map_)
+      if (visited.find(vit.first) == visited.end())
+        if (!DFSVisitHelper(vit.first, visit, visited))
+          return;
+  }
+
+  bool DFSVisit(const V& start, const VisitCallback& visit) const {
+    if (vertex_map_.find(start) == vertex_map_.end())
+      return false;
+
+    std::unordered_set<V> visited;
+    DFSVisitHelper(start, visit, visited);
+    return true;
+  }
+
+  std::string asAdjListString() const {
+    std::string result;
+
+    for (auto& vit: vertex_map_) {
+      result += std::to_string(vit.first);
+      result += ": ";
+
+      for (auto& eit: vit.second) {
+        result += "(";
+        result += std::to_string(eit.first);
+        result += ", ";
+        result += std::to_string(eit.second);
+        result += "), ";
+      }
+
+      result = result.substr(0, result.length() - 2);
+      result += '\n';
+    }
+
+    return result;
+  }
+};
+
+#endif //GRAPH_GRAPH_H
---
+++ data-structures/graph/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "graph.h"
+
+int main() {
+  Graph<int, int> graph;
+  const int size = 20;
+
+  for (int i = 0; i < size; ++i) {
+    bool res = graph.AddVertex(i);
+    std::cout << "adding vertex " << i << " " << (res ? "succeeded." : "failed...") << std::endl;
+  }
+
+  int v = -1, u = -1;
+  for (int i = 0; i < 100; ++i) {
+    v = rand() % (size - 1);
+    u = rand() % (size - 1);
+    bool res = graph.AddEdge(v, u, i);
+    std::cout << "adding edge from " << v << " to " << u << " " << (res ? "succeeded." : "failed...") << std::endl;
+  }
+
+  std::cout << "removing vertex 1 " << (graph.RemoveVertex(1) ? "succeeded." : "failed...") << std::endl;
+  std::cout << "removing vertex 1 " << (graph.RemoveVertex(1) ? "succeeded." : "failed...") << std::endl;
+
+  std::cout << "removing edge (" << v << ", " << u << ") " << (graph.RemoveEdge(v, u) ? "succeeded." : "failed...") << std::endl;
+  std::cout << "removing edge (" << v << ", " << u << ") " << (graph.RemoveEdge(v, u) ? "succeeded." : "failed...") << std::endl;
+
+  graph = Graph<int, int>();
+  graph.AddVertex(1);
+  graph.AddVertex(2);
+  graph.AddVertex(3);
+  graph.AddVertex(4);
+  graph.AddVertex(5);
+  graph.AddVertex(6);
+  graph.AddVertex(7);
+
+  graph.AddEdge(1, 2, 1);
+  graph.AddEdge(1, 3, 1);
+
+  graph.AddEdge(2, 4, 1);
+  graph.AddEdge(2, 5, 1);
+
+  graph.AddEdge(3, 6, 1);
+
+  graph.AddEdge(4, 7, 1);
+  graph.AddEdge(5, 7, 1);
+
+  graph.AddEdge(6, 5, 1);
+  graph.AddEdge(6, 7, 1);
+
+  std::cout << graph.asAdjListString() << std::endl;
+
+  graph.BFS([](const int& val) -> bool {
+    std::cout << val << " ";
+    return true;
+  });
+
+  std::cout << '\n';
+
+  graph.DFS([](const int& val) -> bool {
+    std::cout << val << " ";
+    return true;
+  });
+
+  std::cout << '\n';std::cout << '\n';
+
+  graph.BFSVisit(1, [](const int& val) -> bool {
+    std::cout << val << " ";
+    return true;
+  });
+
+  std::cout << '\n';
+
+  graph.DFSVisit(1, [](const int& val) -> bool {
+    std::cout << val << " ";
+    return val % 2 != 0;
+  });
+
+
+  return 0;
+}
